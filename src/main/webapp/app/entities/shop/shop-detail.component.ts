876d1d2222f9d68e282a244c4ada005488269de1
chegola <arkhomk@yahoo.com>
2017-11-08 09:32:45 +0700

    Show address when click on google map

--- src/main/webapp/app/entities/shop/shop-detail.component.ts
+++ src/main/webapp/app/entities/shop/shop-detail.component.ts
@@ -6,10 +6,26 @@ import { JhiEventManager } from 'ng-jhipster';
 
 import { ResponseWrapper } from '../../shared';
 
+import { Observable } from "rxjs/Observable";
+
 import { Shop } from './shop.model';
 import { ShopService } from './shop.service';
 import { Album } from '../album/album.model';
 import { AlbumService } from '../album/album.service';
+
+interface Address {
+    long_name: string;
+    short_name: string;   
+}
+interface AddressComponent {
+    address_components: Address[];
+    formatted_address: string;
+    types: string[];
+}
+
+interface GeoResult {
+    results: AddressComponent[];
+}
 
 @Component({
     selector: 'jhi-shop-detail',
@@ -22,13 +38,30 @@ export class ShopDetailComponent implements OnInit, OnDestroy {
     private subscription: Subscription;
     private eventSubscriber: Subscription;
     albums: Album[];
-
+    courses: Observable<GeoResult[]>;
+    address: GeoResult[];
     constructor(
         private eventManager: JhiEventManager,
         private shopService: ShopService,
         private route: ActivatedRoute,
-        private albumService: AlbumService
+        private albumService: AlbumService,
+        private httpClient: HttpClient
     ) {
+    }
+
+    onMapClick(event) {
+        console.log('xxxxx: ' + event.coords.lat);
+        this.courses = this.httpClient
+            .get<GeoResult[]>('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + event.coords.lat + ',' + event.coords.lng +
+                '&key=AIzaSyBlk6Nxh8iMaKuhuJK_sv3gFhi_aoeK_Kg&language=th');
+        // .do(console.log);
+        this.courses.subscribe((x) => {
+            console.log(x);
+           
+            this.address = x;
+            console.log(this.address[0]);
+            // address_components[1].short_name
+        });
     }
 
     ngOnInit() {
